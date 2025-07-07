5c834264631b05d905984c276ee04f47c544556c
Olawole Abayomi-Owodunni <olaaba01@e135520.arm.com>
2025-07-07 17:09:01 +0100

    MLECO-6355: Fixing render issue in composable
    * New dependency "libs.androidx.ui.tooling" added to gradle build file to resolve rendering issues
    * All composables have preview to enable easier updates to UI components
    * BottomNavBar is not used so has been removed
    
    Change-Id: I68b626dd894f0b1b739816670da73ef5ca35fd26
    Signed-off-by: Olawole Abayomi-Owodunni <olawole.abayomi-owodunni@arm.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.google.accompanist.permissions)
     testImplementation(libs.junit)
